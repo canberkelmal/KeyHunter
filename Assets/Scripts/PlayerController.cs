f7d3e80b2da0e616c4eedb285ec87e572a92bc37
canberkelmal <elmalcanberk@gmail.com>
2023-11-05 21:14:34 +0300

    Writing attack mechanic.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,8 +9,29 @@ public class PlayerController : MonoBehaviour
     public FloatingJoystick floatingJoystick;
     public Rigidbody rb;
 
+    public bool isRanged = false;
+    public float range = 1f;
+    public bool isAttacking = false;
+    public LayerMask attackableLayerMask;
+    public GameObject attackingObject;
+
     public void FixedUpdate()
     {
+        // Check for any attackable around
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, range, attackableLayerMask);
+        hitColliders = hitColliders.OrderBy(collider => Vector3.Distance(transform.position, collider.transform.position)).ToArray();
+
+        if (hitColliders.Length > 0)
+        {
+            isAttacking = true;
+        }
+        else
+        {
+            isAttacking = false;
+        }
+
+
+        // Movement and rotation
         if (floatingJoystick.Vertical != 0 || floatingJoystick.Horizontal != 0)
         {
             Vector3 direction = new Vector3(floatingJoystick.Horizontal, 0, floatingJoystick.Vertical).normalized;
@@ -19,12 +41,33 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector3(desiredVelocity.x, rb.velocity.y, desiredVelocity.z);
 
             // Rotation
-            Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
-            rb.MoveRotation(targetRotation);
+            if (!isAttacking)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+                rb.MoveRotation(targetRotation);
+                attackingObject = null;
+            }
+            else
+            {
+                Collider closestCollider = hitColliders[0];
+                Quaternion targetRotation = Quaternion.LookRotation(new Vector3(closestCollider.transform.position.x, rb.transform.position.y, closestCollider.transform.position.z) - rb.transform.position, Vector3.up);
+                rb.MoveRotation(Quaternion.Euler(0, targetRotation.eulerAngles.y, 0));
+                AttackTo(closestCollider.gameObject);
+            }
         }
         else
         {
             rb.velocity = Vector3.zero;
+        }
+
+
+    }
+
+    void AttackTo(GameObject targetObject)
+    {
+        if(targetObject != attackingObject)
+        {
+            attackingObject = targetObject;
         }
     }
 }
